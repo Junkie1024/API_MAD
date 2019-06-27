40d0fb9fdeff79fc0d0d6778eb15e31de2365eaa
Modi <1894082@cegepgim.lcl>
2019-06-27 16:26:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1894082/api_mad/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a1894082.api_mad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String ls = getResources().getString(R.string.link);
+
+        try {
+            String mysts =  new Asycdata().execute(ls).get();
+
+            System.out.println("This is frm MainActivity:"+mysts);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
